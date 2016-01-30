2fab10b1c5a705519b31445af732cdb6d5af63ce
Dustin <dustinsinkey1@gmail.com>
2016-01-30 14:52:11 -0800

    Added MainCtrl to controllers.js and save function to modal button

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,12 +1,31 @@
-angular.module('MyApp.controllers', []).controller('BookmarkCtrl', function($scope){
-    $scope.save = function(){
-        var arrBookmarks = BookmarkStore.get();
+angular.module('MyApp.controllers', []).controller('BookmarkCtrl', function($scope, BookmarkStore, $rootScope, $element){
+  $scope.save = function(){
 
-        arrBookmarks.push({
-            url: $scope.bookmark.url,
-            title: $scope.bookmark.title,
-            tags: $scope.bookmark.tags.split(','),
-            created: new Date().getTime()
-        });
-    };
+    var arrBookmarks = BookmarkStore.get();
+
+    arrBookmarks.push({
+      url: $scope.bookmark.url,
+      title: $scope.bookmark.title,
+      tags: $scope.bookmark.tags.split(','),
+      created: new Date().getTime()
+    });
+
+    BookmarkStore.put(arrBookmarks);
+
+    $rootScope.$bookcast('MainCtrl:loadBookmarks', {});
+    $element.modal('hide');
+  };
+});
+
+angular.module('MyApp.controllers', []).controller('MainCtrl', function($scope, BookmarkStore, $location){
+  loadBookmarks();
+
+  $scope.$on('MainCtrl:loadBookmarks', loadBookmarks);
+
+  function loadBookmarks(){
+    $scope.bookmarks = BookmarkStore.get(true);
+    $scope.perPage = 10;
+    $scope.totalPages = Math.ceil($scope.bookmarks.lenght / $scope.perPage);
+    $scope.currentPage = 1;
+  };
 });
