59f4bd4c3651da796702f2cf6e06e88de27b5aa2
Dustin <dustinsinkey1@gmail.com>
2016-01-30 14:05:41 -0800

    Created bookmark array, and pushed results from services.js to it

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -3,7 +3,10 @@ angular.module('MyApp.controllers', []).controller('BookmarkCtrl', function($sco
         var arrBookmarks = BookmarkStore.get();
 
         arrBookmarks.push({
-            
+            url: $scope.bookmark.url,
+            title: $scope.bookmark.title,
+            tags: $scope.bookmark.tags.split(','),
+            created: new Date().getTime()
         });
     };
 });
