052a1c376513f851c98a0337d0d830045bb133f4
Dustin <dustinsinkey1@gmail.com>
2016-01-31 16:46:17 -0800

    Configured ngRoute in app.js and added views/bookmarks file

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
 'use strict';
 
-angular.module('myApp', ['myApp.controllers', 'myApp.services']).run(function($document) { });
+angular.module('myApp', ['ngRoute', 'myApp.controllers', 'myApp.services']).config(function($routeProvider) {
+  $routeProvider.when('/page/:pageNumber', {templateUrl: 'views/bookmarks.html'});
+  $routeProvider.otherwise({redirectTo: '/page/1'});
+});
