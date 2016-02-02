b247bac8fbe80254ed4d1a129ba43a4ba67d67c0
Dustin <dustinsinkey1@gmail.com>
2016-02-01 19:00:59 -0800

    Added modernizr for animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp', ['ngRoute', 'ngTouch', 'myApp.controllers', 'myApp.services']).config(function($routeProvider) {
+angular.module('myApp', ['ngRoute', 'ngAnimate','ngTouch', 'myApp.controllers', 'myApp.services']).config(function($routeProvider) {
   $routeProvider.when('/page/:pageNumber', {templateUrl: 'views/bookmarks.html'});
   $routeProvider.otherwise({redirectTo: '/page/1'});
 });
